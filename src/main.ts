038c8517d9d3d0535d6600d1e6551c4dece90436
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2019-09-07 00:15:03 +0300

    Add helmet

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import {NestExpressApplication} from '@nestjs/platform-express';
 import {join} from 'path';
 import {ValidationPipe} from '@nestjs/common';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+  app.use(helmet());
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
