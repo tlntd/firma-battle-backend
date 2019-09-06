f80230db4e9d6471b832eab4ed9cbaca891d781e
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2019-09-07 00:40:09 +0300

    More Heroku updates

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   app.use(helmet());
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
