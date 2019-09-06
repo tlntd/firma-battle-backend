bb86bfb4767547e1764367f3e0dff333345eded7
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2019-09-06 19:40:49 +0300

    Update scrore interface

--- src/score/interfaces/score.interface.ts
+++ src/score/interfaces/score.interface.ts
@@ -1,6 +1,13 @@
+import {Company} from '../../company/company.entity';
+import {Question} from '../../../dist/question/question.entity';
+
 export interface Score {
   readonly id: number;
-  readonly companyId: number;
-  readonly questionId: number;
-  readonly score: number;
+  company: Company;
+  opponent: Company;
+  question: Question;
+  score: number;
+  delta: number;
+  winner: boolean;
+  createdAt: Date;
 }
